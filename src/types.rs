15d1237ea782f6c102a860240014f2c6ab8fce44
Angry Coding <mailidanielifechukwu@gmail.com>
2024-11-28 23:44:40 +0000

    chore: types for the nor oracle

---
+++ src/types.rs
@@ -0,0 +1,91 @@
+use serde::{Deserialize, Serialize};
+
+
+#[derive(Serialize, Deserialize, Debug)]
+struct SpotifyApiResponse {
+    total: u32,      
+    limit: u32,      
+    offset: u32,     
+    href: String,   
+    next: Option<String>, 
+    previous: Option<String>, 
+    items: Vec<Item>,
+}
+
+
+#[derive(Serialize, Deserialize, Debug)]
+#[serde(untagged)]
+enum Item {
+    Artist(Artist),
+    Track(Track),
+}
+
+
+#[derive(Serialize, Deserialize, Debug)]
+struct ExternalIds {
+    isrc: String, 
+    ean: String,  
+    upc: String,  
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+struct Album {
+    album_type: String,
+    artists: Vec<Artist>,
+    available_markets: Vec<String>,
+    external_urls: ExternalUrls,
+    href: String,
+    id: String,
+    images: Vec<Image>,
+    is_playable: bool,
+    name: String,
+    release_date: String,
+    release_date_precision: String,
+    total_tracks: u32,
+    r#type: String,
+    uri: String,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+struct Artist {
+    external_urls: ExternalUrls,
+    href: String,
+    id: String,
+    name: String,
+    r#type: String,
+    uri: String,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+struct ExternalUrls {
+    spotify: String,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+struct Image {
+    height: u32,
+    url: String,
+    width: u32,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+struct Track {
+    album: Album,
+    artists: Vec<Artist>,
+    available_markets: Vec<String>,
+    disc_number: u32,
+    duration_ms: u32,
+    explicit: bool,
+    external_ids: ExternalIds,
+    external_urls: ExternalUrls,
+    href: String,
+    id: String,
+    is_local: bool,
+    is_playable: bool,
+    name: String,
+    popularity: u32,
+    preview_url: Option<String>,
+    track_number: u32,
+    r#type: String,
+    uri: String,
+}
